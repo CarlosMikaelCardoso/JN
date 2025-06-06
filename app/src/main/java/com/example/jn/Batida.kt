5cf92e8dd7c9d6618acb8f9964d3a5d1c79d8a10
Mikael <mikael.cardoso.costa13@gmail.com>
2025-06-06 18:53:20 -0300

    Correção de layout

--- app/src/main/java/com/example/jn/Batida.kt
+++ app/src/main/java/com/example/jn/Batida.kt
@@ -1,5 +1,9 @@
 package com.example.jn
 
+import com.google.firebase.Timestamp
+import java.util.Date
+
 data class Batida(
-    val items: List<AcaiOutput> = emptyList()
+    val items: List<AcaiOutput> = emptyList(),
+    val timestamp: Timestamp = Timestamp(Date()) // <-- ADICIONE APENAS ESTA LINHA
 )
